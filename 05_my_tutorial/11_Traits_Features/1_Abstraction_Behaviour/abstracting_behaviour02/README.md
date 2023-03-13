25a5c87f95d384a4515c5f127bbe8769842afb76
YoungHa Kim <ytok1108@aol.com>
2023-03-13 22:26:44 +0900

    Abstractiog Behaviour

---
+++ 05_my_tutorial/11_Traits_Features/1_Abstraction_Behaviour/abstracting_behaviour02/README.md
@@ -0,0 +1,7 @@
+# Result
+
+```
+my_title: Name { title: Mr, forename: "Global", surname: "Young" }
+my_title2: "Mrs. global2 hahaha"
+my_title: "Mr. GlobalYoung"
+```
---
+++ 05_my_tutorial/11_Traits_Features/1_Abstraction_Behaviour/abstracting_behaviour02/src/main.rs
@@ -0,0 +1,61 @@
+use std::fmt;
+
+#[derive(Debug)]
+enum Title {
+    Mr,
+    Mrs,
+    Ms,
+    Mx,
+}
+
+
+impl Title {
+    fn to_string(&self) -> String {
+        match self {
+            Title::Mr => "Mr.".to_owned(),
+            Title::Mrs => "Mrs.".to_owned(),
+            Title::Ms => "Ms.".to_owned(),
+            Title::Mx => "Mx.".to_owned(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Name {
+    title: Title,
+    forename: String,
+    surname: String,
+}
+
+impl Name {
+    fn to_string(&self) -> String {
+        let mut buf = String::new();
+
+        buf.push_str(&self.title.to_string());
+        buf.push(' ');
+        buf.push_str(&self.forename);
+        buf.push_str(&self.surname);
+
+        buf
+    }
+}
+
+fn main() {
+    let my_title = Name {
+        title: Title::Mr,
+        forename: "Global".to_string(),
+        surname: "Young".to_string(),
+    };
+
+    let my_title2 = Name {
+        title: Title::Mrs,
+        forename: "global2 ".to_string(),
+        surname: "hahaha".to_string(),
+    };
+    let my_name = "Global Young2";
+    my_title2.to_string();
+
+    println!("my_title: {my_title:?}");
+    println!("my_title2: {:?}", my_title2.to_string());
+    println!("my_title: {:?}", my_title.to_string());
+}
