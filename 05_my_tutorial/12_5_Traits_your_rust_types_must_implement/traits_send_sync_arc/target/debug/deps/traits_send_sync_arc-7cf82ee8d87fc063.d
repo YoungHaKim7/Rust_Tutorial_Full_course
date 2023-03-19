ce90798fe0eb311b19b06d9ad599be80c920d2d6
YoungHa Kim <ytok1108@aol.com>
2023-03-19 19:31:15 +0900

    traits_DB_Arc

--- 05_my_tutorial/12_5_Traits_your_rust_types_must_implement/traits_send_sync_arc/target/debug/deps/traits_send_sync_arc-7cf82ee8d87fc063.d
+++
@@ -1,8 +0,0 @@
-/Users/globalyoung/Documents/test/test/rust/Rust_Tutorial_Full_course/05_my_tutorial/12_5_Traits_your_rust_types_must_implement/traits_send_sync_arc/target/debug/deps/traits_send_sync_arc-7cf82ee8d87fc063.rmeta: src/main.rs Cargo.toml
-
-/Users/globalyoung/Documents/test/test/rust/Rust_Tutorial_Full_course/05_my_tutorial/12_5_Traits_your_rust_types_must_implement/traits_send_sync_arc/target/debug/deps/traits_send_sync_arc-7cf82ee8d87fc063.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
