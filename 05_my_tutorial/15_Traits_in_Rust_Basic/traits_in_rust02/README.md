cccffc34bb07d42ec37b7a27042febec6e158ce8
YoungHa Kim <ytok1108@aol.com>
2023-04-02 18:37:06 +0900

    Traits in Rust 002

---
+++ 05_my_tutorial/15_Traits_in_Rust_Basic/traits_in_rust02/README.md
@@ -0,0 +1,12 @@
+# Result
+
+```
+$ cargo run
+
+   Compiling traits_in_rust02 v0.1.0 (/Users/globalyoung/Documents/test/test/c_lang/Rust_Tutorial_Full_course/05_my_tutorial/15_Traits_in_Rust_Basic/traits_in_rust02)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.27s
+     Running `target/debug/traits_in_rust02`
+
+Tweet summary: @johndoe: Hello World!
+Article summary: (Read more....)
+```
---
+++ 05_my_tutorial/15_Traits_in_Rust_Basic/traits_in_rust02/src/main.rs
@@ -0,0 +1,44 @@
+pub struct NewsArticle {
+    pub author: String,
+    pub headline: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String {
+        String::from("(Read more....)")
+    }
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("@johndoe"),
+        content: String::from("Hello World!"),
+        reply: false,
+        retweet: false,
+    };
+
+    let article = NewsArticle {
+        author: String::from("John Doe"),
+        headline: String::from("The Sky is Falling"),
+        content: String::from("The sky is not actually fallign."),
+    };
+
+    println!("Tweet summary: {}", tweet.summarize());
+    println!("Article summary: {}", article.summarize());
+}
