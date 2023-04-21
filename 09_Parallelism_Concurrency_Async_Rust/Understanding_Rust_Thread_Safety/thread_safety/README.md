07580fcab7dfafe6b44cfb1014b1e163a9c1920e
YoungHa Kim <ytok1108@aol.com>
2023-04-21 20:04:55 +0900

    Good Mutex

--- 09_Parallelism_Concurrency_Async_Rust/Understanding_Rust_Thread_Safety/thread_safety/README.md
+++ 09_Parallelism_Concurrency_Async_Rust/Understanding_Rust_Thread_Safety/thread_safety/README.md
@@ -2,4 +2,11 @@
 
 ```
 
+$ cargo run
+
+   Compiling thread_safety v0.1.0 (/Users/globalyoung/Documents/test/test/rust/html_la/Rust_Tutorial_Full_course/09_Parallelism_Concurrency_Async_Rust/Understanding_Rust_Thread_Safety/thread_safety)
+    Finished dev [unoptimized + debuginfo] target(s) in 2.99s
+     Running `target/debug/thread_safety`
+[src/main.rs:12] rx.recv().unwrap() = 4
+
 ```
--- 09_Parallelism_Concurrency_Async_Rust/Understanding_Rust_Thread_Safety/thread_safety/src/main.rs
+++ 09_Parallelism_Concurrency_Async_Rust/Understanding_Rust_Thread_Safety/thread_safety/src/main.rs
@@ -1,12 +1,12 @@
-use std::sync::{mpsc::channel, Arc, RwLock};
+use std::sync::{mpsc::channel, Arc, Mutex};
 
 pub fn main() {
     let (tx, rx) = channel();
 
-    let x = Arc::new(RwLock::new(tx));
+    let x = Arc::new(Mutex::new(tx));
 
     std::thread::spawn(move || {
-        x.read().unwrap().send(4u8).unwrap();
+        x.lock().unwrap().send(4u8).unwrap();
     });
 
     dbg!(rx.recv().unwrap());
