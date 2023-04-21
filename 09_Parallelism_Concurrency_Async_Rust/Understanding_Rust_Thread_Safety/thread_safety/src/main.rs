bbd1ee30c1e67b45cdba8119b109e6351b5f9f8a
YoungHa Kim <ytok1108@aol.com>
2023-04-21 20:02:27 +0900

    Error Code__Sender<u8>__ cannot be shared between threads safely

--- 09_Parallelism_Concurrency_Async_Rust/Understanding_Rust_Thread_Safety/thread_safety/src/main.rs
+++ 09_Parallelism_Concurrency_Async_Rust/Understanding_Rust_Thread_Safety/thread_safety/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::{mpsc::channel, Arc, RwLock};
+
+pub fn main() {
+    let (tx, rx) = channel();
+
+    let x = Arc::new(RwLock::new(tx));
+
+    std::thread::spawn(move || {
+        x.read().unwrap().send(4u8).unwrap();
+    });
+
+    dbg!(rx.recv().unwrap());
 }
