32a541125862a42fe8a547b8391ef5d98bd4947a
GYoung <ytok1108@aol.com>
2024-01-06 18:21:38 +0900

    struct & impl & fn

---
+++ 03_1hr_Kor_Rust_Introduction/02_Kor_University/method_capsulation/README.md
@@ -0,0 +1,4 @@
+# How to Implement Object-Oriented Programming Concepts in Rust
+
+https://www.makeuseof.com/rust-object-oriented-programming-concepts-how-to-implement/
+
---
+++ 03_1hr_Kor_Rust_Introduction/02_Kor_University/method_capsulation/a01_struct_impl_fn/src/main.rs
@@ -0,0 +1,17 @@
+struct SeaCreature {
+    noise: String,
+}
+
+impl SeaCreature {
+    fn get_sound(&self) -> &str {
+        &self.noise
+    }
+}
+
+fn main() {
+    let creature = SeaCreature {
+        noise: String::from("blub"),
+    };
+
+    println!("{}", creature.get_sound());
+}
