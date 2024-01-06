bba52dd0eb01ef7614cb8d2a915e30ed5a9d7048
GYoung <ytok1108@aol.com>
2024-01-06 18:42:55 +0900

    트레잇 inheritance

---
+++ 03_1hr_Kor_Rust_Introduction/02_Kor_University/trait_polymorphism/a03_trait_inheritance/src/main.rs
@@ -0,0 +1,39 @@
+struct SeaCreature {
+    pub name: String,
+    noise: String,
+}
+
+impl SeaCreature {
+    fn get_sound(&self) -> &str {
+        &self.noise
+    }
+}
+
+trait NoiseMaker {
+    fn make_noise(&self);
+}
+
+trait LoudNoiseMaker: NoiseMaker {
+    fn make_alot_of_noise(&self) {
+        self.make_noise();
+        self.make_noise();
+        self.make_noise();
+    }
+}
+
+impl NoiseMaker for SeaCreature {
+    fn make_noise(&self) {
+        println!("{}", &self.get_sound());
+    }
+}
+
+impl LoudNoiseMaker for SeaCreature {}
+
+fn main() {
+    let creature = SeaCreature {
+        name: String::from("Ferris"),
+        noise: String::from("blub"),
+    };
+
+    creature.make_alot_of_noise();
+}
