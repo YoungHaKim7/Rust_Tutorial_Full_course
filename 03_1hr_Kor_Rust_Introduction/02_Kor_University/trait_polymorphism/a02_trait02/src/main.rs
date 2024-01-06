845e244431567531d9d781b0ec3e83cbe351c3d4
GYoung <ytok1108@aol.com>
2024-01-06 18:39:42 +0900

    트레잇에 구현되 ㄴ 메소드

---
+++ 03_1hr_Kor_Rust_Introduction/02_Kor_University/trait_polymorphism/a02_trait02/src/main.rs
@@ -0,0 +1,34 @@
+struct SeaCreature {
+    pub name: String,
+    noise: String,
+}
+
+impl SeaCreature {
+    fn get_sound(&self) -> &str {
+        &self.noise
+    }
+}
+
+trait NoiseMaker {
+    fn make_noise(&self);
+    fn make_alot_of_noise(&self) {
+        self.make_noise();
+        self.make_noise();
+        self.make_noise();
+    }
+}
+
+impl NoiseMaker for SeaCreature {
+    fn make_noise(&self) {
+        println!("{}", &self.get_sound());
+    }
+}
+
+fn main() {
+    let creature = SeaCreature {
+        name: String::from("Ferris"),
+        noise: String::from("blub"),
+    };
+
+    creature.make_alot_of_noise();
+}
