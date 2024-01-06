f1e1be4e806a298e78d397481528f133966b7351
GYoung <ytok1108@aol.com>
2024-01-06 18:12:31 +0900

    parse__int

---
+++ 03_1hr_Kor_Rust_Introduction/02_Kor_University/a05_string_parse/README.md
@@ -0,0 +1,7 @@
+# Result
+
+```
+42 42
+Hello, world!
+
+```
---
+++ 03_1hr_Kor_Rust_Introduction/02_Kor_University/a05_string_parse/src/main.rs
@@ -0,0 +1,8 @@
+fn main() -> Result<(), std::num::ParseIntError> {
+    let a = 42;
+    let a_string = a.to_string();
+    let b = a_string.parse::<i32>()?;
+    println!("{} {}", a, b);
+    println!("Hello, world!");
+    Ok(())
+}
